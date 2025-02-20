d403b3367025b7ae9e1e90ec1332a6d47aa2bb04
Connor Barrett <connor.steven.barrett@gmail.com>
2025-02-20 03:19:17 -0500

    exclude cli from vercel next build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  webpack: (config, { isServer }) => {
+    // Exclude CLI directory from the build
+    config.module.rules.push({
+      test: /cli[\\/].*\.(ts|tsx|js|jsx)$/,
+      loader: 'ignore-loader'
+    });
+    return config;
+  }
 }
 
 module.exports = withNextIntl(nextConfig);
